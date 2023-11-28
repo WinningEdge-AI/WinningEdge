283724ff5d8ae4ce5152c0abb38a132f2f7c8d23
Peter Sushko <petersushko@gmail.com>
2023-11-28 12:29:41 -0800

    improved pylint score

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,9 +1,8 @@
 """
 This module raw accessed data and outputs a dataframe with all the relevant game info.
 Original data came in ~30,000 raw txt files.
-"""
 
-""" NOTES to Self
+NOTES to Self
 best solution as of 11/5
 save this df in github so we can use it instead
 Figure out how to use process_street_actions for preflop
@@ -15,7 +14,7 @@ import os
 #InteractiveShell.ast_node_interactivity = "all"
 files_path = [os.path.join(r,file) for r,d,f in os.walk(r"C:\Users\Peter\Desktop\Springboard\capstone1\Bought hands\unzipped") for file in f]
 
-def get_hole_cards(game, street_indices, small_blind_name):
+def get_hole_cards(sample_game, street_indices, small_blind_name):
     """
     This function captures hole cards if they are available and correctly assigns them to players
     """
@@ -23,48 +22,50 @@ def get_hole_cards(game, street_indices, small_blind_name):
     hole_cards2 = None
 
     # Get hole cards
-    if 'shows' in str(game[street_indices[-1] + 1]):
-        hole_cards1 = game[street_indices[-1] + 1][-11:-1]
-        hole_cards2 = game[street_indices[-1] + 2][-11:-1]
+    if 'shows' in str(sample_game[street_indices[-1] + 1]):
+        hole_cards1 = sample_game[street_indices[-1] + 1][-11:-1]
+        hole_cards2 = sample_game[street_indices[-1] + 2][-11:-1]
 
     # Assign the hole cards correctly
-    hole_cards1_player = game[street_indices[-1] + 1][0:game[street_indices[-1] + 1].index(' ') + 1]
+    hole_cards1_player = sample_game[street_indices[-1] + 1][0:sample_game[street_indices[-1] + 1].index(' ') + 1]
     if hole_cards1_player == small_blind_name:
-        sb_hole_cards = hole_cards1
-        bb_hole_cards = hole_cards2
+        small_bling_hole_cards = hole_cards1
+        big_blind_hole_cards = hole_cards2
     else:
-        sb_hole_cards = hole_cards2
-        bb_hole_cards = hole_cards1
+        small_bling_hole_cards = hole_cards2
+        big_blind_hole_cards = hole_cards1
 
-    return sb_hole_cards, bb_hole_cards
+    return small_bling_hole_cards, big_blind_hole_cards
 
-def left(s, amount):
+def left(sample_string, amount):
     """
     This is unnecessary but I am using it anyway.
     """
-    return s[:amount]
+    return sample_string[:amount]
 
-def process_street_actions(game, start_index, end_index):
+def process_street_actions(sample_game, start_index, end_index):
     """
-    This function loops over the actions that players made during the game and stores them in a list of tuples
+    This function loops over the actions that players made during the game
+    and stores them in a list of tuples
     """
     street_list = []
-    for i in range(start_index, end_index):
-        if 'folds' in game[i]:
+    for j in range(start_index, end_index):
+        if 'folds' in sample_game[j]:
             thistuple = ('f', 0)
             street_list.append(thistuple)
-        if ('calls' in game[i]) or ('bets' in game[i]) or ('raises' in game[i]):
-            dollar = game[i].index('$')
-            amount = game[i][dollar + 1:-2]
-            first_space = game[i].index(' ')
-            action = game[i][first_space + 1:first_space + 5]
+        if ('calls' in sample_game[j] or 'bets' in sample_game[j] or 'raises' in sample_game[j]):
+            dollar_sign = sample_game[j].index('$')
+            amount = sample_game[j][dollar_sign + 1:-2]
+            first_space = sample_game[j].index(' ')
+            action = sample_game[j][first_space + 1:first_space + 5]
             tuple_to_add = (action[0], amount)
             street_list.append(tuple_to_add)
-        if 'checks' in game[i]:
+        if 'checks' in sample_game[j]:
             thistuple = ('k', 0)
             street_list.append(thistuple)
     return street_list
 
+# pylint: disable=C0103
 folded_pre=0
 rows_list=[]
 
@@ -78,7 +79,7 @@ for f in files_path:
 
     # Extract game data based on indices
     games = [lines[game_indices[i-1]:game_indices[i]] for i in range(1, len(game_indices))]
-  
+
     # Now, process each game individually
     for index, game in enumerate(games):
         try:
@@ -110,7 +111,7 @@ for f in files_path:
                 elif line.startswith('** Summary **'):
                     street_indeces.append(index_in_game)
 
-#getting hole cards using the function     
+#getting hole cards using the function
             sb_hole_cards, bb_hole_cards = get_hole_cards(game, street_indeces, small_blind_name)
 
 #Getting preflop actions
@@ -148,7 +149,7 @@ for f in files_path:
                 river_card = game[street_indeces[2]][-5:-3]
                 river=process_street_actions(game, street_indeces[2], street_indeces[3])
 
-            remove_folded_pre =False  #add this parameter to drop hands that were immediately folded by SB to reduce the DF
+            remove_folded_pre =False  #this parameter identifies hands that were folded immediately
             if preflop[0][0][0]=='f':
                 folded_pre+=1
                 remove_folded_pre=True
@@ -162,9 +163,9 @@ for f in files_path:
                          'Player BB':big_blind_name, 'Preflop actions':preflop,
                          'Flop actions':flop, 'Turn actions':turn,'River actions':river,
                          'Flop':flop_cards, 'Turn':turn_card, 'River':river_card, 'SB stack':stack_sb,
-                         'BB stack': stack_bb, 'SB cards': sb_hole_cards, 'BB cards':bb_hole_cards,
-                         'Folded pre':remove_folded_pre}
+                         'BB stack': stack_bb, 'SB cards': sb_hole_cards,
+                         'BB cards':bb_hole_cards,'Folded pre':remove_folded_pre}
             rows_list.append(game_row)
 
-        except:
-            pass
+        except Exception as e:
+            print(f"An error occurred: {e}")
